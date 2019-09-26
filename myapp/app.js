645d4ffcc153014f9cdba5bb38f55c4700f9ec83
alex <a@a.com>
2019-09-26 10:01:06 -0500

    Add balance service

--- myapp/app.js
+++ myapp/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -26,6 +27,9 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+
+
 
 // error handler
 app.use(function(err, req, res, next) {
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
-/* GET home page. */
+
+let count = 0;
+let list = ['10.4.72.150:3000','10.4.74.230:3000','10.4.72.201:3000'];
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/balance', function (req, res) {
+    if(count < 3){
+        axios.get(list[count] + "/users").then(response => {
+            count++;
+            res.json(response.data);
+        }).catch(error => {
+            res.send("Error");
+        });
+    }else{
+    	count = 0;
+    }
 });
 
 module.exports = router;
